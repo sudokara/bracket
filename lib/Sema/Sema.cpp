84e84c0fbf138a6cae6563a57a2959822696eac5
Shrikara A <shrikarame@gmail.com>
2025-03-16 17:26:55 +0530

    feat: semantic analysis v1 wip
    Assumes variables can be shadowed within the body of the let

--- lib/Sema/Sema.cpp
+++ lib/Sema/Sema.cpp
@@ -5,6 +5,7 @@
 namespace {
 class ProgramCheck : public ASTVisitor {
   bool HasError;
+  llvm::StringSet<> ScopedVariables;
 
 public:
   ProgramCheck() : HasError(false) {}
@@ -19,11 +20,20 @@ public:
   };
 
   virtual void visit(Expr &Node) override {
-    if (llvm::isa<Prim>(Node)) {
+    // uses the classof method to check the type of the node
+    if (llvm::isa<Prim>(Node)) { // expr is a primitive
       Node.accept(*this);
       return;
     }
-    if (llvm::isa<Int>(Node)) {
+    if (llvm::isa<Int>(Node)) { // expr is an integer
+      Node.accept(*this);
+      return;
+    }
+    if (llvm::isa<Var>(Node)) { // expr is a variable
+      Node.accept(*this);
+      return;
+    }
+    if (llvm::isa<Let>(Node)) { // expr is a let expression
       Node.accept(*this);
       return;
     }
@@ -47,7 +57,31 @@ public:
     }
   }
 
-  virtual void visit(Int &Node) override { return; }
+  virtual void visit(Int &Node) override { return; } // no semantic check for integer
+
+  virtual void visit(Var &Node) override {
+    if (!ScopedVariables.count(Node.getName())) { // check that the variable is in scope
+      llvm::errs() << "Error: Variable " << Node.getName() << " is not defined\n";
+      HasError = true;
+    }
+  }
+
+  virtual void visit(Let &Node) override {
+    if (Node.getBinding())
+      Node.getBinding()->accept(*this); // accept the binding expression first, no errors in assignment
+
+    bool shadowed = ScopedVariables.count(Node.getVarName()); // true if a variable of same name was defined previously
+
+    ScopedVariables.insert(Node.getVarName()); // add the variable to the scope
+
+    if (Node.getBody())
+      Node.getBody()->accept(*this); // accept the body expression
+
+    // if this variable does not shadow one with the same name
+    // remove this from the scope after the body expression is checked
+    if (!shadowed)
+      ScopedVariables.erase(Node.getVarName()); // remove the variable from the scope 
+  }
 };
 } // namespace
 
